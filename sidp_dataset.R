ee56d612db510cd9497b0d81070fca79bf64df2b
aszucs <szuecs.anna@gmail.com>
2017-04-29 15:00:35 +0200

    updated code in R with legend for abreviations

--- sidp_dataset.R
+++ sidp_dataset.R
@@ -13,10 +13,23 @@ library(R.matlab)
 library(xtable)
 library(Hmisc)
 
-setwd('C:/Users/a/Google Drive/skinner/projects_analyses/sidp')
+setwd('C:\Users\a\Documents\GitHub\sidp')
 
 sidp <- read_excel("sidp.xlsx")
 View(sidp)
+
+##abreviations used:
+## dob = date of birth
+## uppsp = impulsive behavior scale
+## barratt = barratt impulsivity scale
+## spsi = social problem solving inventory
+## mcq = monetary choice questionnaire: delayed discounting
+## doi = decision-outcome inventory: lifetime, 10 past years
+## admc = decision-making competence: sunk cost (sc), admc framing (f)
+## exit = executive interview (neuropsych)
+## ma = McArthur social status: questions 1 to 7
+## anx = anxiety: lifetime, current
+## subst = substance: lifetime, current
 
 #creating a smaller dataset without the suspicious participant coded "5" for subgroup.
 ssidp <- sidp[(sidp$subgroup !=5),]
@@ -25,7 +38,7 @@ ssidp <- sidp[(sidp$subgroup !=5),]
 ssidp <- transform(ssidp, lethality = as.numeric(lethality), antisoc.ref = as.numeric(antisoc.ref), bpd.ref = as.numeric(bpd.ref), narc.ref = as.numeric(narc.ref), ocpd.ref = as.numeric(ocpd.ref))
 ssidp <- transform(ssidp, id = as.factor(id), subgroup.comment = as.factor(subgroup.comment), subgroup = as.factor(subgroup), gender = as.factor(gender), ethnicity = as.factor(ethnicity), race = as.factor(race), anx.life = as.factor(anx.life), anx.current = as.factor(anx.current), subst.life = as.factor(subst.life), subst.current = as.factor(subst.current))
 
-save(sidp,file="sidp.Rda")
+#save(sidp,file="sidp.Rda")
 
 
 #counting number of persons for each subgroup
@@ -128,3 +141,70 @@ summary(f.avoid)
 f.allpd = lm(ssidp$allpd ~ ssidp$subgroup + ssidp$age.baseline + ssidp$gender)
 summary(f.allpd)
 
+
+#linear model for each personality disorder, including age, gender and level of education
+d.narc = lm(ssidp$narc ~ ssidp$education + ssidp$age.baseline + ssidp$gender)
+summary(d.narc)
+
+d.bpd = lm(ssidp$bpd ~ ssidp$education + ssidp$age.baseline + ssidp$gender)
+summary(d.bpd)
+
+d.ocpd = lm(ssidp$ocpd ~ ssidp$education + ssidp$age.baseline + ssidp$gender)
+summary(d.ocpd)
+
+d.antisoc = lm(ssidp$antisoc ~ ssidp$education + ssidp$age.baseline + ssidp$gender)
+summary(d.antisoc)
+
+d.schtyp = lm(ssidp$schtyp ~ ssidp$educatio  + ssidp$age.baseline + ssidp$gender)
+summary(d.schtyp)
+
+d.avoid = lm(ssidp$avoid ~ ssidp$education + ssidp$age.baseline + ssidp$gender)
+summary(d.avoid)
+
+d.allpd = lm(ssidp$allpd ~ ssidp$education + ssidp$age.baseline + ssidp$gender)
+summary(d.allpd)
+
+
+#linear model for each personality disorder, including delay discounting, age and gender
+g.narc = lm(ssidp$narc ~ ssidp$mcq + ssidp$age.baseline + ssidp$gender)
+summary(g.narc)
+
+g.bpd = lm(ssidp$bpd ~ ssidp$mcq + ssidp$age.baseline + ssidp$gender)
+summary(g.bpd)
+
+g.ocpd = lm(ssidp$ocpd ~ ssidp$mcq + ssidp$age.baseline + ssidp$gender)
+summary(g.ocpd)
+
+g.antisoc = lm(ssidp$antisoc ~ ssidp$mcq + ssidp$age.baseline + ssidp$gender)
+summary(g.antisoc)
+
+g.schtyp = lm(ssidp$schtyp ~ ssidp$mcq + ssidp$age.baseline + ssidp$gender)
+summary(g.schtyp)
+
+g.avoid = lm(ssidp$avoid ~ ssidp$mcq + ssidp$age.baseline + ssidp$gender)
+summary(g.avoid)
+
+g.allpd = lm(ssidp$allpd ~ ssidp$mcq + ssidp$age.baseline + ssidp$gender)
+summary(g.allpd)
+
+#linear model for each personality disorder, including sunk cost, age and gender
+g2.narc = lm(ssidp$narc ~ ssidp$admc.sc + ssidp$age.baseline + ssidp$gender)
+summary(g2.narc)
+
+g2.bpd = lm(ssidp$bpd ~ ssidp$admc.sc + ssidp$age.baseline + ssidp$gender)
+summary(g2.bpd)
+
+g2.ocpd = lm(ssidp$ocpd ~ ssidp$admc.sc + ssidp$age.baseline + ssidp$gender)
+summary(g2.ocpd)
+
+g2.antisoc = lm(ssidp$antisoc ~ ssidp$admc.sc + ssidp$age.baseline + ssidp$gender)
+summary(g2.antisoc)
+
+g2.schtyp = lm(ssidp$schtyp ~ ssidp$admc.sc + ssidp$age.baseline + ssidp$gender)
+summary(g2.schtyp)
+
+g2.avoid = lm(ssidp$avoid ~ ssidp$admc.sc + ssidp$age.baseline + ssidp$gender)
+summary(g2.avoid)
+
+g2.allpd = lm(ssidp$allpd ~ ssidp$admc.sc + ssidp$age.baseline + ssidp$gender)
+summary(g2.allpd)
